5e944c64ae7b323b071bf1c22dea17397eaba7aa
Aaron <aaron@unm.edu>
2014-04-30 11:20:43 -0600

    test snapshot in version history

---
+++ index.R
@@ -0,0 +1,28 @@
+---
+title       : Aaron Baca 
+subtitle    : Slidify test 1 
+author      : ramnathv
+job         : 1
+framework   : io2012        # {io2012, html5slides, shower, dzslides, ...}
+highlighter : highlight.js  # {highlight.js, prettify, highlight}
+hitheme     : tomorrow      # 
+widgets     : []            # {mathjax, quiz, bootstrap}
+mode        : selfcontained # {standalone, draft}
+---
+
+## Read-And-Delete
+
+1. Edit YAML front matter
+2. Write using R Markdown
+3. Use an empty line followed by three dashes to separate slides!
+
+--- .class #id 
+
+## Slide 2
+
+* Line 1
+* Line 2
+* Line 3
+
+
+
